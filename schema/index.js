cad240581e372f15fae25f6fbc152b2cefc242b5
Dan Balaban <dbalaban@lampix.co>
2019-04-12 09:49:27 +0300

    GraphQL hello world

---
+++ schema/index.js
@@ -0,0 +1,25 @@
+// import type helpers from graphql-js
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql');
+
+// The root query type is where in the data graph
+// we can start asking questions
+const RootQueryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'world'
+    }
+  }
+});
+
+const ncSchema = new GraphQLSchema({
+  query: RootQueryType,
+  // mutations:
+});
+
+module.exports = ncSchema;
